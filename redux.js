dba69af963274dd059b0509fc2692908e32b3755
aul14 <rahmanaulia336@gmail.com>
2022-10-14 10:33:56 +0700

    Memahami konsep Redux (Dispatch&Subscription)Redux

--- redux.js
+++ redux.js
@@ -4,14 +4,35 @@ const createStore = redux.createStore;
 
 const initialState = {
   value: 0,
+  age: 21,
 };
+
 // REDUCER (FUNGSINYA UNTUK MENGUPDATE STORE)
 const rootReducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case "ADD_AGE":
+      return {
+        ...state,
+        age: state.age + 1,
+      };
+    case "CHANGE_VALUE":
+      return {
+        ...state,
+        value: (state.value = action.newValue),
+      };
+    default:
+      return state;
+  }
 };
+
 // STORE
 const store = createStore(rootReducer);
 console.log(store.getState());
-// DISPATCH OR ACTION (PROSES PEMANGGILAN REDUCER)
 
 // SUBSCRIPTION (PROSES PEMANGGILAN/PENGGUNAAN STORE YG DIBUTUHKAN)
+store.subscribe(() => console.log("store change: ", store.getState()));
+
+// DISPATCH OR ACTION (PROSES PEMANGGILAN REDUCER)
+store.dispatch({ type: "ADD_AGE" });
+store.dispatch({ type: "CHANGE_VALUE", newValue: 12 });
+console.log(store.getState());
